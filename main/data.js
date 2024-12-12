d7133b89adf1ec5bb14933f9a4374e1330f2b3fb
chiuan114514 <huangdi980425@gmail.com>
2024-12-12 10:51:34 +0800

    0

--- main/data.js
+++ main/data.js
@@ -221,7 +221,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
